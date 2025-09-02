db34eba091f505bcb0d54a4c1127788ecce4a063
Dee Yanong <deeyanong@gmail.com>
2025-09-02 14:16:34 -0400

    Table 1 finally worked

--- DnD-Monsters.R
+++ DnD-Monsters.R
@@ -1,3 +1,3 @@
 monsters <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-27/monsters.csv')
 
-readr::write_rds(monsters, here::here("data", "DnD-Monsters.rds"))
+readr::write_rds(monsters, here::here("data", "Monsters.rds"))
